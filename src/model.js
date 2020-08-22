65a951343807f3ee7c64e537be6a8168279499ba
Yaltaphil <yaltaphil@yandex.ru>
2020-08-22 11:50:41 +0300

    feat:model added

---
+++ src/model.js
@@ -0,0 +1,11 @@
+export const model = [
+  { type: "title", value: "Test title", options: {} },
+  {
+    type: "text",
+    value:
+      "Lorem ipsum dolor sit amt, consectetur adipisicing elit. Alias at corporis cupiditate dolo    doloremque fuga incidun",
+    options: {},
+  },
+  { type: "textColumns", value: ["1 text", "2 text", "3 text","4 text"], options: {} },
+  { type: "title", value: "Test title", options: {} },
+];
